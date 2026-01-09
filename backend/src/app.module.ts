ff598aaaacb3ffd592fbc4640a6cfc9efc8090d6
Kouame Mathurin Yao <kouame16.yao@uvci.edu.ci>
2026-01-09 16:18:05 +0000

    configuration du backend avec PostgreSQL et TypeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TagController } from './tag/tag.controller';
+import { TagModule } from './tag/tag.module';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { ArticleController } from './article/article.controller';
+import { ArticleModule } from './article/article.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TagModule,
+    UserModule,
+    ArticleModule,
+  ],
+  controllers: [AppController, TagController, ArticleController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
